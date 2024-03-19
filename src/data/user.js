b99364f15d4e8aa27757145c754ad0dcaee02355
Klay-Michael Dehaney <kdehaney53@gmail.com>
2024-03-19 18:19:52 -0400

    pull/project updates

--- src/data/user.js
+++ src/data/user.js
@@ -1,84 +1,80 @@
 const INFO = {
-	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
-		logo: "../logo.png",
-	},
+    main: {
+        title: "",
+        name: "Klay-Michael Dehaney.",
+        email: "dehaney.klaymichael@gmail.com",
+        logo: "../pic.jpg",
+    },
 
-	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
-	},
+    socials: {
+        github: "https://github.com/KlayD51",
+        linkedin: "https://www.linkedin.com/in/klay-michael-dehaney/",
+    },
 
-	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
-		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
-	},
+    homepage: {
+        title: "Front-end Web Developer.",
+        description:
+            "I am a frontend web developer with an interest in backend, with expertise in Node.js and Javascript libraries like React and Next.js.  Through my experiences at internships like the NBA Digital Product team I have been able to get experience with testing, cloud development, and building quality/efficient code.  I am eager to continue to develop my skills through problem solving and opportunities to grow. ",
+    },
 
-	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
-		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
-	},
+    about: {
+        title: "Aspiring Developer",
+        description:
+            "I’ve worked on a variety of projects and have been able to contribute to some pretty cool repos.  Many of my projects have been in the name of better understanding topics like Api’s, frameworks, and general skills.  Take a look at my Github to see some of them!  I was introduced to Open-Source early in my journey and have maintained that interest as my journey progressed.",
+    },
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-	},
+    articles: {
+        title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+        description:
+            "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+    },
 
-	projects: [
-		{
-			title: "Project 1",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+    projects: [
+        {
+            title: "BMW Blog Page",
+            description:
+                "This project was made to test the ideas of react components, routing, props, and css styling.  One of my first projects blending interest and application.",
+            logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+            linkText: "View Project",
+            link: "https://react-bmwblog.netlify.app",
+        },
 
-		{
-			title: "Project 2",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+        {
+            title: "Flappy Capsule",
+            description:
+                "This project tests the idea of python machine learning, while bringing back a throwback game like Flappy Birds.",
+            logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+            linkText: "View Project",
+            link: "https://github.com/KlayD51/FlappyBirdAi",
+        },
 
-		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+        {
+            title: "TMDB Theater",
+            description:
+                "This project demonstrates API usage, object parsing, and react props.  Blending in the problem of last minute movie sourcing.",
+            logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+            linkText: "View Project",
+            link: "https://github.com/KlayD51/MovieApp",
+        },
 
-		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-	],
+        {
+            title: "MongPress",
+            description:
+                "This project demonstrates API communication with no-sql database via server requests(express).",
+            logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+            linkText: "View Project",
+            link: "https://github.com/KlayD51/MongPress/tree/main",
+        },
+/*
+        {
+            title: "Project 5",
+            description:
+                "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+            logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+            linkText: "View Project",
+            link: "https://github.com",
+        },*/
+    ],
 };
 
 export default INFO;
